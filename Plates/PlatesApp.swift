84dd8779d7a1c0ceaf7567c6ffdc4e5ad3a9b7f7
Anson Wan <anson.hy.wan@gmail.com>
2022-02-24 18:24:15 -0500

    Initial Commit

---
+++ Plates/PlatesApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlatesApp.swift
+//  Plates
+//
+//  Created by Anson Wan on 2022-02-24.
+//
+
+import SwiftUI
+
+@main
+struct PlatesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
